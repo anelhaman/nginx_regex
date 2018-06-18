02f0197171115c12f54038b8c7443cca4dc5b65a
anelhaman <anelhaman@hotmail.com>
2018-06-18 18:10:11 +0700

    20180618180959

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 docker run -d \
 --name nginx \
 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
--v $(pwd)/40x.html:/var/www/html/errors/40x.html \
+-v $(pwd)/404.html:/var/www/html/errors/404.html \
 -v $(pwd)/50x.html:/var/www/html/errors/50x.html \
 -p 8000:80 nginx
